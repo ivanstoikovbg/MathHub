efe174f3bcb63d23d163887da0e460b3c7fb9ff3
Ivan Stoikov <97913067+IvanStoikov07@users.noreply.github.com>
2024-10-05 17:36:21 +0300

    add new files

---
+++ KvadratnoUravnenie/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KvadratnoUravnenie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            double c;
+
+            while (true)
+            {
+                Console.Write("Въведете (a): ");
+                a = double.Parse(Console.ReadLine());
+
+                Console.Write("Въведете (b): ");
+                b = double.Parse(Console.ReadLine());
+
+                Console.Write("Въведете (c): ");
+                c = double.Parse(Console.ReadLine());
+
+                double d = b * b - 4 * a * c;
+                Console.WriteLine($"Дискриминантата е {d}");
+
+                if (d < 0)
+                {
+                    Console.WriteLine("Няма реални корени!");
+                }
+                else if (d == 0)
+                {
+                    double xEqual = -b / (2 * a);
+                    Console.WriteLine($"Двата корена са равни: X = {xEqual}");
+                }
+                else
+                {
+                    double x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                    double x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                    Console.WriteLine($"X1 = {x1}, X2 = {x2}");
+                }
+
+                Console.Write("Искате ли да изчислите ново уравнение? (Да/Не): ");
+                string answer = Console.ReadLine();
+                if (answer.ToLower() != "да")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ TriangleCalculator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Въведете страната A: ");
+                double a = double.Parse(Console.ReadLine());
+                Console.Write("Въведете страната B: ");
+                double b = double.Parse(Console.ReadLine());
+                Console.Write("Въведете страната C: ");
+                double c = double.Parse(Console.ReadLine());
+
+                double obikolka = a + b + c;
+                double s = obikolka / 2;
+                double lice = Math.Sqrt(s * (s - a) * (s - b) * (s - b) * (s - c));
+
+                Console.WriteLine($"Обиколката е равна на {obikolka}");
+                Console.WriteLine($"Лицето е равно на {lice}");
+
+                if (a == b && b == c)
+                {
+                    Console.WriteLine("Триъгълника е равностранен.");
+                }
+                else if (a == b || b == c || a == c)
+                {
+                    Console.WriteLine("Триъгълника е равнобедрен.");
+                }
+                else if (a + a + b + b == c * c || a + a + c + c == b * b || b + b + c + c == a * a)
+                {
+                    Console.WriteLine("Триъгълникът е правоъгълен.");
+                }
+            }
+        }
+    }
+}
